7b3e771981af7318fb2ae5362e13da7e703c9e6e
filodoks <filodoks@gmail.com>
2019-08-31 18:14:33 +0200

    dodany koment

--- src/cokolwiek/company/Main.java
+++ src/cokolwiek/company/Main.java
@@ -2,33 +2,40 @@ package cokolwiek.company;
 
 import java.io.*;
 
+/*
+
+Nazwa pliku do sumowania powinna byc podana w wierszu polecen jako
+argument wywolania aplikacji
+
+ */
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Wywolanie programu: Suma nazwa_pliku");
+        if(args.length < 1){
+            System.out.println("Wywołanie programu: Suma nazwa_pliku");
             return;
         }
 
         StreamTokenizer strTok = null;
-        try {
+        try{
             strTok = new StreamTokenizer(
                     new BufferedReader(
                             new FileReader(args[0])
                     )
             );
-        } catch (FileNotFoundException e) {
-            System.out.println("Nie moge otworzyc pliku: " + args[0]);
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Nie mogę otworzyć pliku: " + args[0]);
             return;
         }
-
         double suma = 0;
-        try {
-            while (strTok.nextToken() != StreamTokenizer.TT_EOF) {
+        try{
+            while(strTok.nextToken() != StreamTokenizer.TT_EOF){
                 if(strTok.ttype == StreamTokenizer.TT_NUMBER)
                     suma += strTok.nval;
             }
-        } catch (IOException e) {
-            System.out.println("Blad I/O podczas odczytu pliku.");
+        }
+        catch(IOException e){
+            System.out.println("Błąd I/O podczas odczytu pliku.");
             return;
         }
         System.out.println("Wynik: " + suma);
